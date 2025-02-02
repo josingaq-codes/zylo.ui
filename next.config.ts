11bb997c822e65ed5270f4f859b63da759b0324d
José Leonel Inga Quintana <josingaq.codes@gmail.com>
2025-02-02 14:17:43 -0500

    Zylo UI v1.25

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  images: {
-    domains: ["digital.webhop.net"],
-  },
 };
 
 export default nextConfig;
